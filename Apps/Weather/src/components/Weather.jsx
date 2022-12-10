14e6bdce72478e52637a8d6799175c663f7b71da
madan3242 <madangowda3242@gmail.com>
2022-12-10 10:57:37 +0530

    WeatherApp

---
+++ Apps/Weather/src/components/Weather.jsx
@@ -0,0 +1,50 @@
+import Axios from 'axios'
+import React, {useState} from 'react'
+import './css/Weather.css'
+import WeatherCard from './WeatherCard'
+
+const Weather = () => {
+    const [location, setLocation]=useState("")
+    const [weather, setWeather]=useState({})
+    
+    const getWeather = () => {
+        let API_KEY = '08fd635a4b9b9fc85c38820e54fe2521'
+        let URL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=metric`
+
+        Axios.post(URL)
+             .then(res => {
+                setWeather(res.data)
+                // console.log(res.data);
+             })
+             .catch(() => {
+                console.log('Incorrect');
+             })
+    }
+
+    const search = (e) => {
+        if(e.key === 'Enter'){
+            getWeather();
+        }
+    }
+
+  return (
+    <div className="bg">
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-10 mx-auto">
+                    <div className="form-group">
+                        <input className="form-control p-4" type="text" placeholder="Enter your city name" onChange={e => setLocation(e.target.value)} onKeyDown={search}/>
+                    </div>
+                </div>
+            </div>
+        </div>
+        {
+        Object.keys(weather).length > 0 ? <>
+            <WeatherCard weather={weather} />
+        </> : <></>
+        }
+    </div>
+  )
+}
+
+export default Weather
---
+++ Apps/Weather/src/components/WeatherCard.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const WeatherCard = (props) => {
+    let weather = props.weather;
+    const icon = weather.weather[0].icon;
+    const url = `http://openweathermap.org/img/w/${icon}.png`
+    return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-10 mx-auto">
+                        <div className="card">
+                            {/* <pre>{JSON.stringify(weather)}</pre> */}
+                            <img className="mx-auto" src={url} alt="" width="80px" />
+                            <h2 className="mx-auto">{weather.name} {weather.sys.country}</h2>
+                            <h2 className="mx-auto">Weather: {weather.weather[0].description}</h2>
+                            <h2 className="mx-auto">Temprature: {weather.main.temp}°C</h2>
+                            <h2 className="mx-auto">Humidity: {weather.main.humidity}% </h2>
+                            <h2 className="mx-auto">Wind speed: {weather.wind.speed}Km/h</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    )
+}
+
+export default WeatherCard
